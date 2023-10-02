d5a4c62ef0834f083cc6e5147b900a70158e2632
Dimitar Mihaylov <dimitar.mihaylove@gmail.com>
2023-10-02 21:57:48 +0300

    finish quiz 2

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
@@ -39,6 +37,24 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+
+
+        if (scores.contains_key(&team_1_name)) {
+            let team1 = scores.get_mut(&team_1_name).unwrap();
+            team1.goals_conceded+= team_2_score;
+            team1.goals_scored+= team_1_score;
+        } else {
+            scores.insert(team_1_name, Team{goals_scored: team_1_score, goals_conceded: team_2_score});
+        }
+
+        if (scores.contains_key(&team_2_name)) {
+            let team2 = scores.get_mut(&team_2_name).unwrap();
+            team2.goals_conceded+= team_1_score;
+            team2.goals_scored+= team_2_score;
+        } else {
+            scores.insert(team_2_name, Team{goals_scored: team_2_score, goals_conceded: team_1_score});
+        }
+
     }
     scores
 }
